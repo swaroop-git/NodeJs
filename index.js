058307db3d2413240bb15d274c5b29d6c65b7912
Lute <swaroop.madhukar-lute@capgemini.com>
2024-09-08 14:08:53 +0530

    creating server having separate function

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
-// import express from "express";
+// // import express from "express";
 
-const express = require('express');   //import express dependency
+// const express = require('express');   //import express dependency
 
-const app = express();      //initiate express app 
-const port = 5000;          // save the port number where you will be listening
+// const app = express();      //initiate express app 
+// const port = 5000;          // save the port number where you will be listening
 
-//Routes HTTP GET requests to the specified path with the specified callback functions
-app.get('/', function (req,res) {
-    // res.send('GET request to homepage');
-    res.sendFile('index.html', {root: __dirname});   // server responds by sending the index.html file to the client's browser
-    //the .sendFile method needs the absolute path to the file
-});
+// //Routes HTTP GET requests to the specified path with the specified callback functions
+// app.get('/', function (req,res) {
+//     // res.send('GET request to homepage');
+//     res.sendFile('index.html', {root: __dirname});   // server responds by sending the index.html file to the client's browser
+//     //the .sendFile method needs the absolute path to the file
+// });
 
-app.listen(port, () => {
-    console.log(`Now listening on ${port}`);
-});
+// app.listen(port, () => {
+//     console.log(`Now listening on ${port}`);
+// });
 
 // p1 = Promise.resolve(50);
 // p2 = 200
@@ -52,9 +52,57 @@ app.listen(port, () => {
 // method setTimeout ( function, milliseconds, string) ------> delay an operation
 // method setInterval(function, milliseconds, param1, param2, ...)
 
-const myfunc = delay => {
-    console.log('hello ' + delay + ' seconds');
-}
+// const myfunc = delay => {
+//     console.log('hello ' + delay + ' seconds');
+// }
 
-setTimeout(myfunc , 50, 'five');
-setTimeout(myfunc, 100, 'Ten');
+// setTimeout(myfunc , 50, 'five');
+// setTimeout(myfunc, 100, 'Ten');
+
+
+/*********************************************************************************************888****/
+// const express = require('express');
+// const app = express();
+
+// const port = 4000;
+
+// app.post('/post', function (req, res) {
+//     res.send('index.html', {root : __dirName});
+// })
+
+// app.get('/get', function (req,res) {
+//    res.sendFile('index.html', {root : __dirname}, ()=>{
+//     if(err){
+//         err.statusCode;
+//         console.log('err');
+//     }
+//    });
+// });
+/************************************************************************************************8888 */
+
+// // CREATING separate function 
+
+// const express = require('express');
+// const app = express();
+
+// const port = 4000;
+
+// const func = (req, res) => {
+//     res.sendFile('index.html', {root: __dirname}, ()=>{
+//         if (err) {
+//             console.log('ERROR');
+//         }else {
+//             console.log('SUCCESS');
+//         }
+//     })
+// };
+
+// app.get('/', func);
+
+// app.listen(port, ()=>{
+//    console.log(`listening to ${port}`); 
+// });
+
+
+/***********************************************************************************************8 */
+
